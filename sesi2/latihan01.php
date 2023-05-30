ca025cba7047a5789f67410168648b500c28947d
mariaindrafa <134986659+mariaindrafa@users.noreply.github.com>
2023-05-30 15:29:50 +0700

    Add files via upload

---
+++ sesi2/latihan01.php
@@ -0,0 +1,9 @@
+
+<?php
+    echo "Belajar Script PHP"; //diakhiri tanda;//
+    $dta = "Vita"; //$variabel dan "untuk tipe data string"//
+    echo '<br> Nama $dta'; //menggunakan petik tunggal hanya mencetak tulisannya tanpa isi variabelnya
+    echo '<br>Nama' .$dta; //menggunakan petik tunggal dan dipisah variabelnya
+    echo "<br>Nama: $dta"; //tanda petik ganda menampilkan isi variabelnya
+    $angka =5; //variabel berisi angka tidak menggunakan tanda petik
+    echo '<br>Nama:' .$dta. " Peringkat ke-" .$angka ;
